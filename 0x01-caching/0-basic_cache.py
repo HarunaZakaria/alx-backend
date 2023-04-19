d8c6bd74a3e6ec045049d17c0b6cc2f11ce1deb1
harunaZakaria <Harunzy55@gmail.com>
2023-04-19 15:15:54 +0000

    add more

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -19,7 +19,7 @@ def get(self, key):
 Must return the value in self.cache_data linked to key.
 If key is None or if the key doesn’t exist in self.cache_data, return None.
 """
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -34,7 +34,7 @@ class BasicCache(BaseCaching):
         """
         Initialize the class using the parent class __init__ method
         """
-        BaseCaching.__init__(self)
+        super().__init__()
 
     def put(self, key, item):
         """
