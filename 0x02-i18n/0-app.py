0a8bc647e02a1e57e73ccb357bc5627228347106
harunaZakaria <Harunzy55@gmail.com>
2023-04-26 12:05:24 +0000

    add

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_templete
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello_world():
+    return render_templete("0-index.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
